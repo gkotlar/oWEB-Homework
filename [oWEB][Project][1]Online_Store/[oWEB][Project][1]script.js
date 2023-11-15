9bc4185cc3ca0b115e9c13474c98557452f3133f
gkotlar <147694259+gkotlar@users.noreply.github.com>
2023-11-15 23:58:54 +0100

    lastpush

--- [oWEB][Project][1]Online_Store/[oWEB][Project][1]script.js
+++ [oWEB][Project][1]Online_Store/[oWEB][Project][1]script.js
@@ -1,1 +1,22 @@
+function setImage() { //js for the picture change
+    if (numFalseGuesses <= numMaxGuesses) {
+        iconImg = document.getElementById("hangmanPic");
+        iconImg.setAttribute("src", "resourses/pictures/hangman" + numFalseGuesses + ".png");
+        iconImg.setAttribute("alt", "hangman" + numFalseGuesses);
+    }
+}
+// window.addEventListener("click", start, false);
 
+function start() {
+    let person = prompt("Please enter your username", "Harry ");
+    let pass = prompt("Please enter your password", " Potter");
+}
+var counter = 0;
+function heartchange() {
+    if (document.getElementById("Heart1").src == "Resourses/heartEmpty.png") {
+        document.getElementById("Heart1").src = "Resourses/heartFull.png";
+    }
+    else {
+        document.getElementById("Heart1").src = "Resourses/heartEmpty.png";
+    }
+}
