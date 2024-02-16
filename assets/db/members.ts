67f5270151bb915b6a3a228509a8398c3ae18eca
gkotlar <147694259+gkotlar@users.noreply.github.com>
2024-02-16 03:22:13 +0100

    Add files via upload

---
+++ assets/db/members.ts
@@ -0,0 +1,53 @@
+import { Person } from "../../app/models/person";
+
+export const MEMBERS: Person[] = [
+    {
+        picture:"/",
+        name:"Gjorgji Kotlarovski",
+        type:"0",
+        role:"Secretary",
+        mail:"gjotgji.kotlarovski@best-eu.org",
+        link:"https://www.linkedin.com/in/gjorgji-kotlarovski/",
+    },
+    {
+        picture:"/",
+        name:"test non boardie",
+        type:"1",
+        role:"vivaldi",
+        mail:"",
+        link:"",
+    },
+    {
+        picture:"/",
+        name:"test MO",
+        type:"2",
+        role:"motweek",
+        mail:"/",
+        link:"/",
+    },
+    {
+        picture:"",
+        name:"full member",
+        type:"3",
+        role:"full member",
+        mail:"",
+        link:"",
+    },
+    {
+        picture:"",
+        name:"almnis",
+        type:"4",
+        role:"alumni",
+        mail:"",
+        link:"",
+    },
+    {
+        picture:"/",
+        name:"second boradie",
+        type:"0",
+        role:"testtest",
+        mail:"/",
+        link:"/",
+    },
+    
+];
