9c0bc1f860b1b039ea76b19fadc6a1a295dd90e4
gkotlar <147694259+gkotlar@users.noreply.github.com>
2024-02-16 02:23:32 +0100

    idk

---
+++ [oWEB][Project][2]Website_Student_org/best-sk-angular/dist/best-sk-angular/browser/assets/db/social-media-list.ts
@@ -0,0 +1,40 @@
+import { Entity } from "../../app/models/entity";
+
+export const SOCIALMEDIA: Entity[] = [
+    {
+        type: "social",
+        picture: "assets/svg/facebook.svg",
+        link: "https://www.facebook.com/bestskopje/",
+        name: "Facebook"
+    },
+    {
+        type: "social",
+        picture: "assets/svg/instagram.svg",
+        link: "https://www.instagram.com/bestskopje/",
+        name: "Instagram"
+    },
+    {
+        type: "social",
+        picture: "assets/svg/linkedin.svg",
+        link: "https://www.linkedin.com/company/best-skopje/",
+        name: "LinkedIn"
+    },
+    {
+        type: "social",
+        picture: "assets/svg/discord.svg",
+        link: "https://discord.com/invite/NFMpaZB",
+        name: "Discord"
+    },
+    {
+        type: "social",
+        picture: "assets/svg/youtube.svg",
+        link: "https://www.youtube.com/user/bestskopje",
+        name: "Youtube"
+    },
+    {
+        type: "social",
+        picture: "assets/svg/camera-fill.svg",
+        link: "https://www.flickr.com/photos/bestskopje",
+        name: "Flickr"
+    },
+];
---
+++ [oWEB][Project][2]Website_Student_org/best-sk-angular/dist/best-sk-angular/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-3ZMHUNGY.mjs";import"./chunk-YB2KE7CH.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
